b2bcb44fe9c3ae9339a1fba3225d8bc9c61d8213
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2023-11-23 21:39:34 +0100

    Adjust copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,28 +1,40 @@
 return {
   {
-
     "zbirenbaum/copilot.lua",
     enabled = true,
+    autostart = true,
     cmd = "Copilot",
     event = "InsertEnter",
+    -- config = function() require("copilot").setup {} end,
     opts = {
-      suggestion = { enabled = true },
-      panel = { enabled = false },
-    },
-  },
-  {
-    "nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-      {
-        "zbirenbaum/copilot-cmp",
-        opts = {},
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          next = "<C-j>",
+          prev = "<C-k>",
+        },
       },
     },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require "cmp"
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot" }, { name = "emoji" } }))
-    end,
+    -- opts = {
+    --   suggestion = { enabled = true },
+    --   panel = { enabled = false },
+    -- },
   },
+  -- {
+  --   "nvim-cmp",
+  --   dependencies = {
+  --     "hrsh7th/cmp-emoji",
+  --     {
+  --       "zbirenbaum/copilot-cmp",
+  --       opts = {},
+  --     },
+  --   },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     local cmp = require "cmp"
+  --     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot" }, { name = "emoji" } }))
+  --   end,
+  -- },
 }
